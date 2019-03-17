bc7f27937df988f0847fb508196c2c2462513a05
wp-usercamp <41485636+wp-usercamp@users.noreply.github.com>
2019-03-17 14:24:03 +0100

    better js

--- assets/js/frontend/usercamp.js
+++ assets/js/frontend/usercamp.js
@@ -1,5 +1,52 @@
 jQuery( function( $ ) {
 
+	// Global function.
+	var usercamp = {
+
+		// Toggle password visibility.
+		togglepw: function( el ) {
+			if ( el.hasClass( 'is-hidden' ) ) {
+				el.addClass( 'is-visible' ).removeClass( 'is-hidden' ).attr( 'data-tip', el.attr( 'data-hide') ).html( '<i data-feather=eye></i>' ).parents( '.usercamp-field' ).find( 'input[type=password]' ).attr( 'type', 'text' ).focus();
+			} else {
+				el.addClass( 'is-hidden' ).removeClass( 'is-visible' ).attr( 'data-tip', el.attr( 'data-show') ).html( '<i data-feather=eye-off></i>' ).parents( '.usercamp-field' ).find( 'input[type=text]' ).attr( 'type', 'password' ).focus();
+			}
+			$( document.body ).trigger( 'uc-init-tooltips' );
+			feather.replace();
+		},
+
+		// Trigger form.
+		triggerform: function( el ) {
+			el.addClass( 'disabled' ).parents( 'form' ).submit();
+		},
+
+		// Submit form.
+		sendform: function( el ) {
+			if ( el.attr( 'data-ajax' ) != 'yes' ) {
+				return true;
+			}
+			e.preventDefault();
+
+			$.post( usercamp_params.ajaxurl, el.serialize() + '&action=usercamp_send_form&id=' + el.attr( 'data-id' ), function(response) {
+				el.find( '.usercamp-button.main' ).removeClass( 'disabled' );
+				el.find( '.usercamp-error, .usercamp-message, .usercamp-info' ).remove();
+				el.find( '.uc-error' ).removeClass( 'uc-error' );
+				el.prepend( response.html );
+				if ( response.error_fields ) {
+					$.each( response.error_fields, function( i, e ) {
+						el.find(e).find( 'label, input' ).addClass( 'uc-error' );
+					} );
+				} else {
+					el.find( ':input:not([type=hidden])' ).val( '' );
+				}
+			} ).fail( function(xhr, status, error) {
+				el.find( '.usercamp-button.main' ).removeClass( 'disabled' );
+				el.find( '.usercamp-error, .usercamp-message, .usercamp-info' ).remove();
+				el.prepend( '<div class="usercamp-info">' + error + '</div>' );
+			} );
+		},
+
+	};
+
 	// Tooltips
 	$( document.body ).on( 'uc-init-tooltips', function() {
 		$( '.usercamp-help-tip, .tips' ).tipTip( {
@@ -20,50 +67,17 @@ jQuery( function( $ ) {
 		} )
 
 		.on( 'click', '.uc-pw-visible', function(e) {
-			c = $( this );
-			if ( c.hasClass( 'is-hidden' ) ) {
-				c.addClass( 'is-visible' ).removeClass( 'is-hidden' ).attr( 'data-tip', c.attr( 'data-hide') ).html( '<i data-feather=eye></i>' ).parents( '.usercamp-field' ).find( 'input[type=password]' ).attr( 'type', 'text' ).focus();
-			} else {
-				c.addClass( 'is-hidden' ).removeClass( 'is-visible' ).attr( 'data-tip', c.attr( 'data-show') ).html( '<i data-feather=eye-off></i>' ).parents( '.usercamp-field' ).find( 'input[type=text]' ).attr( 'type', 'password' ).focus();
-			}
-			$( document.body ).trigger( 'uc-init-tooltips' );
-			feather.replace();
+			usercamp.togglepw( $( this ) );
 		} )
 
 		// Send form
 		.on( 'click', '.usercamp-button.main:not(.disabled)', function(e) {
-			var f = $( this );
-			f.addClass( 'disabled' );
-			f.parents( 'form' ).submit();
+			usercamp.triggerform( $( this ) );
 		} )
 
 		// Submit form
 		.on( 'submit', '.usercamp form', function(e) {
-			var t = $( this );
-
-			if ( t.attr( 'data-ajax' ) != 'yes' ) {
-				return true;
-			}
-			e.preventDefault();
-
-			$.post( usercamp_params.ajaxurl, t.serialize() + '&action=usercamp_send_form&id=' + t.attr( 'data-id' ), function(response) {
-				t.find( '.usercamp-button.main' ).removeClass( 'disabled' );
-				t.find( '.usercamp-error, .usercamp-message, .usercamp-info' ).remove();
-				t.find( '.uc-error' ).removeClass( 'uc-error' );
-				t.prepend( response.html );
-				if ( response.error_fields ) {
-					$.each( response.error_fields, function( i, e ) {
-						t.find(e).find( 'label, input' ).addClass( 'uc-error' );
-					} );
-				} else {
-					t.find( ':input:not([type=hidden])' ).val( '' );
-				}
-			} ).fail( function(xhr, status, error) {
-				t.find( '.usercamp-button.main' ).removeClass( 'disabled' );
-				t.find( '.usercamp-error, .usercamp-message, .usercamp-info' ).remove();
-				t.prepend( '<div class="usercamp-info">' + error + '</div>' );
-			} );
-
+			usercamp.sendform( $( this ) );
 		} );
 
 });
