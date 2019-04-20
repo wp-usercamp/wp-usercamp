d388ec4a9355b694c59e16d3546b5947e44959b3
wp-usercamp <41485636+wp-usercamp@users.noreply.github.com>
2019-04-20 15:46:23 +0100

    move profile function to profiles

--- includes/uc-profile-functions.php
+++ includes/uc-profile-functions.php
@@ -26,3 +26,20 @@ function uc_get_profile_endpoints() {
 function uc_get_active_profile_id() {
 	return apply_filters( 'uc_get_active_profile_id', username_exists( esc_attr( get_query_var( 'uc_user' ) ) ) );
 }
+
+/**
+ * Get a user profile URL.
+ */
+function usercamp_get_profile_url( $username = '', $ajax = false ) {
+	global $the_user;
+	if ( empty( $username ) ) {
+		$username = $the_user->get( 'user_login', 'edit' );
+	}
+	$permalink = uc_get_page_permalink( 'profile' );
+
+	if ( $ajax ) {
+		$username = '<span class="uc-ajax">' . $username . '</span>';
+	}
+
+	return apply_filters( 'usercamp_get_profile_url', uc_get_endpoint_url( $username, '', $permalink ), $username, $the_user );
+}
